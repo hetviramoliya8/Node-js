d4d12b7ddccf73455550ecf52586e65f94c8c3b3
hetviramoliya8 <ramoliyahetvi8@gmail.com>
2026-02-24 09:35:44 +0530

    project

--- JWT with react/Backend/middlewares/mailer.js
+++ JWT with react/Backend/middlewares/mailer.js
@@ -3,7 +3,7 @@ const nodemailer = require("nodemailer")
 const transport = nodemailer.createTransport({
     service : "gmail",
     auth:{
-        user :"jensipatel369@gmail.com",
+        user :"hetviramoliya8@gmail.com",
         pass : "vgoevqkxmayumuca"
     }
 })
@@ -12,7 +12,7 @@ const transport = nodemailer.createTransport({
 module.exports.sendOtp = (email,otp)=>{
     let mailoptions = {
         to : email,
-        from : "jensipatel369@gmail.com",
+        from : "hetviramoliya8@gmail.com",
         subject : "Password Reset OTP",
         text : `Your password reset otp is ${otp}`
     }
